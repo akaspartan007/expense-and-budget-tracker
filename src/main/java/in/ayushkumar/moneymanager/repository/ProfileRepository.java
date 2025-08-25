ec4452616a4e4e69c0c3e2c818b0c5e9c578f2e9
akaspartan007 <134705392+akaspartan007@users.noreply.github.com>
2025-08-25 18:17:33 +0530

    First commit - uploading till frist milestone

---
+++ src/main/java/in/ayushkumar/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,14 @@
+package in.ayushkumar.moneymanager.repository;
+
+import in.ayushkumar.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    Optional<ProfileEntity> findByEmail(String email);
+
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+
+}
---
+++ src/main/java/in/ayushkumar/moneymanager/service/AppUserDetailService.java
@@ -0,0 +1,29 @@
+package in.ayushkumar.moneymanager.service;
+
+import in.ayushkumar.moneymanager.entity.ProfileEntity;
+import in.ayushkumar.moneymanager.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailService implements UserDetailsService {
+    private final ProfileRepository profileRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        ProfileEntity existingProfile = profileRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Profile not found with Email: " + email));
+        return User.builder()
+                .username(existingProfile.getEmail())
+                .password(existingProfile.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
